228064acad1a3711edcba3bd9791b8749bfcdee8
kimown <kimown@163.com>
2016-06-10 20:03:21 +0800

    添加下express

--- src/main/java/duokan/aop/ServiceMonitor.java
+++ src/main/java/duokan/aop/ServiceMonitor.java
@@ -24,10 +24,8 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 
 @Aspect
-@Component
 public class ServiceMonitor {
 
     private static final Logger logger = LoggerFactory
--- src/main/java/duokan/controller/SampleController.java
+++ src/main/java/duokan/controller/SampleController.java
@@ -3,26 +3,28 @@ package duokan.controller;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
 import duokan.domain.DuokanBookInfo;
-import duokan.mapper.DuokanBookInfoMapper;
 import duokan.service.CrudService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by kimown on 16-6-4.
  */
 @Controller
+@RequestMapping(value = "/",produces = MediaType.APPLICATION_JSON_UTF8_VALUE,consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
 public class SampleController {
 
     @Autowired
@@ -31,10 +33,9 @@ public class SampleController {
     private static final Logger logger = LoggerFactory
             .getLogger(SampleController.class);
 
-    @RequestMapping("/")
+    @RequestMapping(value = "", method = RequestMethod.GET,consumes = MediaType.ALL_VALUE)
     @ResponseBody
-    String home()  {
-
+    String home() {
 
 
         Path path = Paths.get("tempforSerialize.json");
@@ -44,10 +45,17 @@ public class SampleController {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        List<DuokanBookInfo> empList = JSON.parseObject(stringList1.get(0), new TypeReference<List<DuokanBookInfo>>(){} );
+        List<DuokanBookInfo> empList = JSON.parseObject(stringList1.get(0), new TypeReference<List<DuokanBookInfo>>() {
+        });
         crudService.crud();
 
         return "Hello World!";
+    }
+
+    @RequestMapping(value = "/setjson", method = RequestMethod.POST )
+    @ResponseBody
+    public String set2Db (@RequestBody String json) {
+        return "ok";
     }
 
 }
